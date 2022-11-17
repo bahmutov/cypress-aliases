ece77a516c5c89663dc9862286f2c2b4b06fbb69
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-17 12:18:49 -0500

    more testing

--- cypress/e2e/should.cy.js
+++ cypress/e2e/should.cy.js
@@ -4,3 +4,13 @@ it('replaces the aliases value', () => {
   cy.wrap(42).as('answer')
   cy.wrap(20 + 22).should('equal', '@answer')
 })
+
+it('ignores unknown aliases', () => {
+  cy.wrap(42).as('answer')
+  cy.wrap('@unknown').should('equal', '@unknown')
+})
+
+it('ignores alias names that don not start with @', () => {
+  cy.wrap(42).as('answer')
+  cy.wrap('answer').should('equal', 'answer')
+})
