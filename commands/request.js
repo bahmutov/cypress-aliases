72aed50fade6c107214074d00a4565755ed25375
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-11-17 17:15:39 -0500

    support the method overwrite

--- commands/request.js
+++ commands/request.js
@@ -1,11 +1,17 @@
 // @ts-check
 
-Cypress.Commands.overwrite('request', (request, url) => {
+Cypress.Commands.overwrite('request', (request, method, url) => {
+  console.log({ method, url })
+  if (typeof url === 'undefined') {
+    url = method
+    // @ts-ignore
+    method = 'GET'
+  }
+
   if (typeof url === 'string') {
     // @ts-ignore
     const aliases = cy.state('aliases')
     Cypress._.forEach(aliases, (alias, key) => {
-      console.log('checking', { key, alias })
       // @ts-ignore
       url = url.replaceAll('@' + key, aliases[key].subject)
     })
--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -12,3 +12,29 @@ it('cy.request URL', () => {
       title: 'Post 2',
     })
 })
+
+it('cy.request URL several aliases', () => {
+  cy.wrap('posts').as('resource')
+  cy.wrap(2).as('postId')
+  cy.request(
+    'https://my-json-server.typicode.com/typicode/demo/@resource/@postId',
+  )
+    .its('body')
+    .should('deep.equal', {
+      id: 2,
+      title: 'Post 2',
+    })
+})
+
+it('cy.request METHOD URL', () => {
+  cy.wrap(2).as('postId')
+  cy.request(
+    'GET',
+    'https://my-json-server.typicode.com/typicode/demo/posts/@postId',
+  )
+    .its('body')
+    .should('deep.equal', {
+      id: 2,
+      title: 'Post 2',
+    })
+})
